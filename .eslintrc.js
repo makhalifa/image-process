8a1f5a91b49bdfa009c69188d4af5b6457c89164
Mohamed Khalifa <7mdiano@gmail.com>
2022-12-21 00:55:26 +0200

    add prettier and eslint files

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'prettier'],
+  rules: {
+    semi: ['error', 'never'],
+    quotes: ['error', 'single'],
+    'prettier/prettier': 1, // 0: off, 1: warning, 2: error
+    'no-console': 1,
+    'no-var': 'error',
+    'prefer-const': 'error',
+  },
+}
